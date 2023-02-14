f574770b7b0548e8ffadc1afa40cd43ff0fa1b25
sinmxdr <sinyjjl@gmail.com>
2023-02-15 02:07:28 +0800

    mv ProductCallManage.vue

--- oa-front/src/views/inventory/index.vue
+++ oa-front/src/views/inventory/index.vue
@@ -11,10 +11,17 @@
         </el-divider>
         <purchasingManage />
     </div>
+    <div style="margin-top: 30px;">
+        <el-divider content-position="left">
+            <h2>库存警报</h2>
+        </el-divider>
+        <productCallManage />
+    </div>
 </template>
 
 <script setup>
 import { user } from '@/pinia/modules/user'
 import taskManage from './TaskManage.vue'
 import purchasingManage from './PurchasingManage.vue'
+import productCallManage from './ProductCallManage.vue'
 </script>
--- oa-front/src/views/purchasing/index.vue
+++ oa-front/src/views/purchasing/index.vue
@@ -5,15 +5,8 @@
         </el-divider>
         <purchasingManage />
     </div>
-    <div style="margin-top: 30px;">
-        <el-divider content-position="left">
-            <h2>库存警报</h2>
-        </el-divider>
-        <productCallManage />
-    </div>
 </template>
 
 <script setup>
 import purchasingManage from './PurchasingManage.vue'
-import productCallManage from './ProductCallManage.vue'
 </script>
